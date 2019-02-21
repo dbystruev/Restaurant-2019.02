a239ab8af1cbdb11e81e49f49ee69bff7f891271
Denis Bystruev <dbystruev@me.com>
2019-02-21 20:20:13 +0300

    Add MenuTableViewController

---
+++ Restaurant/Controllers/MenuTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MenuTableViewController.swift
+//  Restaurant
+//
+//  Created by Denis Bystruev on 18/02/2019.
+//  Copyright © 2019 Denis Bystruev. All rights reserved.
+//
+
+import UIKit
+
+class MenuTableViewController: UITableViewController {
+
+}
